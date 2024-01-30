f8ce3c0c135913c6102f8b5a58f6ea47ba38d8d7
SeungYeopO <95047108+SeungYeopO@users.noreply.github.com>
2024-01-30 21:13:34 +0900

    Update 1012.cpp

--- 1012.cpp
+++ 1012.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
 int t, m, n, k;
@@ -9,15 +10,25 @@ queue<pair<int, int>>q;
 int dy[4] = { -1, 0, 1, 0 };
 int dx[4] = { 0, 1, 0, -1 };
 
+void init() {
+	for (int i = 0; i < 52; i++) {
+		for (int j = 0; j < 52; j++) {
+			map[i][j] = 0;
+			visited[i][j] = 0;
+		}
+	}
+}
+
 void bfs(int y, int x) {
 
-	visited[y][x] = true;
+	visited[y][x] = 1;
 	q.push({ y, x });
 
 
 	while (!q.empty()) {
 		int now_y = q.front().first;
 		int now_x = q.front().second;
+
 		q.pop();
 
 		for (int i = 0; i < 4; i++) {
@@ -28,11 +39,12 @@ void bfs(int y, int x) {
 			if (visited[next_y][next_x]) continue;
 			if (map[next_y][next_x] == 0) continue;
 
-			visited[next_y][next_x] = true;
+			visited[next_y][next_x] = 1;
 			q.push({ next_y, next_x });
 		}
 	}
-	
+
+	return;
 }
 
 int main() {
@@ -41,12 +53,26 @@ int main() {
 
 	cin >> t;
 	for (int tc = 1; tc <= t; tc++) {
+		init();
 		cin >> m >> n >> k;
 		for (int i = 0; i < k; i++) {
 			int x, y;
 			cin >> x >> y;
 			map[y][x] = 1;
 		}
+
+		int cnt = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] && !visited[i][j]) {
+					bfs(i, j);
+					cnt++;
+				}
+			}
+		}
+
+		cout << cnt << endl;
 	}
 
 
