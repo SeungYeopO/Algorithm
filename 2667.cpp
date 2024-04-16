da81ed2d5e5719c691fc716dfaa96d713b85325d
SeungYeopO <95047108+SeungYeopO@users.noreply.github.com>
2024-04-16 23:49:55 +0900

    240416 - 1 solved

--- 2667.cpp
+++ 2667.cpp
@@ -1,69 +1,78 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//	ios::sync_with_stdio(false);
-//	cin.tie(0);	cout.tie(0);
-//
-//}
-
 #include <iostream>
 #include <queue>
+#include <string>
 #include <algorithm>
-
 using namespace std;
-#define MAX 26
 
-int n, cnt = 0;
-string arr[MAX];
-bool visited[MAX][MAX] = { 0, };
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-vector<int> result;
-queue<pair<int, int>> q;
+int N;
+int map[31][31];
+int visited[31][31];
+queue<pair<int, int>>q;
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
+int dat[666];
+string str;
 
-void bfs(int x, int y) {
-	q.push({ x,y });
-	visited[x][y] = true;
-	cnt++;
-
+void bfs(int y, int x, int num) {
+	int cnt = 1;
+	q.push({ y, x });
+	visited[y][x] = num;
 	while (!q.empty()) {
-		int a = q.front().first;
-		int b = q.front().second;
+		int cur_y = q.front().first;
+		int cur_x = q.front().second;
 		q.pop();
+
 		for (int i = 0; i < 4; i++) {
-			int nx = a + dx[i];
-			int ny = b + dy[i];
-			if (0 <= nx && 0 <= ny && nx < n && ny < n && visited[nx][ny] == false && arr[nx][ny] == '1') {
-				q.push({ nx,ny });
-				visited[nx][ny] = true;
-				cnt++;
-			}
+			int next_y = cur_y + dy[i];
+			int next_x = cur_x + dx[i];
+
+			if (next_y < 0 || next_x < 0 || next_y >= N || next_x >= N) continue;
+			if (map[next_y][next_x] == 0) continue;
+			if (visited[next_y][next_x]) continue;
+
+			q.push({ next_y, next_x });
+			visited[next_y][next_x] = num;
+			++cnt;
 		}
 	}
+	dat[num] = cnt;
+
+	return;
 }
-
 int main() {
-	cin >> n;
+	ios::sync_with_stdio(false);
+	cin.tie(0);	cout.tie(0);
 
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (arr[i][j] == '1' && visited[i][j] == false) {
-				cnt = 0;
-				bfs(i, j);
-				result.push_back(cnt);
-			}
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> str;
+		for (int j = 0; j < str.length(); j++) {
+			map[i][j] = str[j] - '0';
 		}
 	}
 
-	sort(result.begin(), result.end());
+	int cnt = 0;
 
-	cout << result.size() << "\n";
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i] << "\n";
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] > 0 && visited[i][j] == 0) {
+				cnt++;
+				bfs(i, j, cnt);
+				
+			}
+
+		}
 	}
+
+	cout << cnt << endl;
+	sort(dat, dat + 666);
+
+	for (int i = 0; i < 666; i++) {
+		if (dat[i] > 0) {
+			cout << dat[i] << endl;
+		}
+	}
+	
+
+	return 0;
 }
