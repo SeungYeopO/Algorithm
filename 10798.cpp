a95b58cfaa22c53a48e4bdc8bace319ee333990d
SeungYeopO <95047108+SeungYeopO@users.noreply.github.com>
2024-04-25 09:41:17 +0900

    240425 2 solved

---
+++ 10798.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+char arr[5][15];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	char ch;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (arr[j][i] != NULL)
+				cout << arr[j][i];
+		}
+	 }
+
+
+	return 0;
+}
